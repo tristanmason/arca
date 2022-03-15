67b2813f2eea09a36b655d6d5c157d52b2f87106
Tristan Mason <24866055+tristanmason@users.noreply.github.com>
2022-03-14 23:13:11 -0500

    Inner Container block and styles. PHP code cleanup in other blocks.

--- template-parts/blocks/flexispacer/flexispacer.php
+++ template-parts/blocks/flexispacer/flexispacer.php
@@ -11,14 +11,14 @@
 
 // Create id attribute allowing for custom "anchor" value.
 $id = 'flexispacer-' . $block['id'];
-if( !empty($block['anchor']) ) {
-    $id = $block['anchor'];
+if ( !empty($block['anchor']) ) {
+	$id = $block['anchor'];
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
 $className = 'flexispacer';
-if( !empty($block['className']) ) {
-    $className .= ' ' . $block['className'];
+if ( !empty($block['className']) ) {
+	$className .= ' ' . $block['className'];
 }
 
 // Load values and assign defaults.
@@ -30,17 +30,18 @@ $min_height_desktops = get_field('min_height_desktops');
 ?>
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>"></div>
 <style>
-    #<?php echo esc_attr($id); ?>{
-        height:calc(<?php echo $flexi_min . 'px + ' . $flexi_vw . 'vw'?>);
-    }
-    <?php if ($min_height_tablets) : ?>@media (min-width:600px) {
-        #<?php echo esc_attr($id); ?>{
-            height:calc(<?php echo $min_height_tablets . 'px + ' . $flexi_vw . 'vw'?>);
-        }
-    }<?php endif; ?>
-    <?php if ($min_height_desktops) : ?>@media (min-width:1200px) {
-        #<?php echo esc_attr($id); ?>{
-            height:calc(<?php echo $min_height_desktops . 'px + ' . $flexi_vw . 'vw'?>);
-        }
-    }<?php endif; ?>
+	#<?php echo esc_attr( $id ); ?>{
+		height:calc(<?php echo $flexi_min . 'px + ' . $flexi_vw . 'vw'?>);
+	}
+	<?php if ( $min_height_tablets ) : ?>
+		@media (min-width:600px) {
+		#<?php echo esc_attr( $id ); ?>{
+			height:calc(<?php echo $min_height_tablets . 'px + ' . $flexi_vw . 'vw'?>);
+		}
+	}<?php endif; ?>
+	<?php if ($min_height_desktops) : ?>@media (min-width:1200px) {
+		#<?php echo esc_attr( $id ); ?>{
+			height:calc(<?php echo $min_height_desktops . 'px + ' . $flexi_vw . 'vw'?>);
+		}
+	}<?php endif; ?>
 </style>
---
+++ template-parts/blocks/innercontainer/innercontainer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ArCa Inner Container Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'innercontainer-' . $block['id'];
+if ( ! empty( $block['anchor'] ) ) {
+	$id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$className = 'innercontainer';
+if ( ! empty( $block['className'] ) ) {
+	$className .= ' ' . $block['className'];
+}
+
+// Template for default inner blocks
+$template = array(
+	array(
+		'core/paragraph',
+		array(
+			'content' => 'Any blocks (like this one) inside the Inner Container will be limited to the maximum width you set. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		),
+	),
+);
+
+// Load values and assign defaults.
+$container_max_width = get_field( 'container_max_width' );
+$container_width_unit = get_field( 'container_width_unit' );
+?>
+<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $className ); ?>" style="max-width:<?php echo esc_attr( $container_max_width ) . esc_attr( $container_width_unit ); ?>;margin:0 auto;padding:0 1em;">
+	<?php echo '<InnerBlocks template="' . esc_attr( wp_json_encode( $template ) ) . '" />'; ?>
+</div>
