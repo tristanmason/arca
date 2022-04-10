e2a325b115cea997fa5986f76a7a36c1dc64dbed
Tristan Mason <24866055+tristanmason@users.noreply.github.com>
2022-04-10 08:43:55 -0500

    fix issue with arca-angles images overflowing block editor wrapper

--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function theme_enqueue_styles() {
 	$theme_scripts = "/js/child-theme{$suffix}.js";
 
 	wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . $theme_styles, array(), $the_theme->get( 'Version' ) );
-	wp_enqueue_script( 'jquery' );
+	// wp_enqueue_script( 'jquery' ); // Not needed
 	wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . $theme_scripts, array(), $the_theme->get( 'Version' ), true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
